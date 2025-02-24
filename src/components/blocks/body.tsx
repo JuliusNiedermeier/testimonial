1bd6218291526fd9b44060938f32afecde5f6c1f
JuliusNiedermeier <julius.niedermeier@outlook.de>
2025-02-24 18:06:19 +0100

    Removed deprecated block components

--- src/components/blocks/body.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from "react";
-import { BlockMap } from ".";
-import { cn } from "@/utils/cn";
-
-export const BodyBlock: FC<BlockMap["body"]> = ({ config }) => {
-  return <p className={cn("text-foreground-secondary")}>{config.content}</p>;
-};
--- src/components/blocks/single-line-text-input.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-import { BlockMap } from ".";
-import { Input } from "../primitives/input";
-
-export const SingleLineTextInputBlock: FC<
-  BlockMap["single-line-text-input"]
-> = ({ config }) => {
-  return <Input placeholder={config.placeholder} />;
-};
