20df706ea3e29d88736db020ce4a83833cf66613
JuliusNiedermeier <julius.niedermeier@outlook.de>
2025-02-26 22:03:50 +0100

    Implemented basic video feedback

---
+++ src/components/form/steps/video-feedback/index.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import { FC, useEffect, useRef } from "react";
+import { useForm } from "../..";
+import { useRecorder } from "./video-recorder";
+import { cn } from "@/utils/cn";
+
+const formatDuration = (durationInSeconds: number) => {
+  const minutes = String(Math.floor(durationInSeconds / 60)).padStart(2, "0");
+  const seconds = String(durationInSeconds % 60).padStart(2, "0");
+  return `${minutes}:${seconds}`;
+};
+
+export const VideoFeedbackStep: FC<{ questionId: string }> = ({
+  questionId,
+}) => {
+  const { spaceConfig, testimonial, updateTestimonial, getQuestion } =
+    useForm();
+  if (!spaceConfig) return null;
+
+  const { question, index } = getQuestion(questionId);
+  if (!question) return null;
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const recordedVideo = testimonial?.answers[questionId].video;
+
+  const recorder = useRecorder({
+    enabled: !recordedVideo,
+    onVideo: (video) => {
+      updateTestimonial({ answers: { [questionId]: { video } } });
+    },
+  });
+
+  const handleRecordControlClick = () => {
+    if (recordedVideo) {
+      updateTestimonial({ answers: { [questionId]: { video: undefined } } });
+    } else recorder?.recording ? recorder?.stop() : recorder?.start();
+  };
+
+  // Handle switching video source between recorded video and live stream
+  useEffect(() => {
+    if (!videoRef.current) return;
+    if (recordedVideo) {
+      videoRef.current.srcObject = null;
+      videoRef.current.src = URL.createObjectURL(recordedVideo);
+    } else {
+      videoRef.current.src = "";
+      videoRef.current.srcObject = recorder?.stream || null;
+    }
+  }, [recorder?.stream, recordedVideo, recorder?.recording]);
+
+  return (
+    <div>
+      <span className="text-label text-foreground-secondary">
+        {index + 1}/{spaceConfig.questions.length}
+      </span>
+      <h1 className="text-label mt-4">{question.content}</h1>
+      <div className="aspect-square relative rounded-md bg-background-secondary mt-10 grid overflow-hidden">
+        <div className="relative h-full w-full">
+          <video
+            ref={videoRef}
+            controls={false}
+            muted={!recordedVideo}
+            autoPlay={!recordedVideo}
+            playsInline
+            className="w-full h-full object-cover -scale-x-100"
+          />
+          <div className="absolute bottom-4 left-0 right-0 flex flex-col items-center gap-4 pointer-events-none">
+            <button
+              className={cn(
+                "size-20 rounded-full border-[3px] border-background-primary grid place-items-center transition-colors pointer-events-auto",
+                {
+                  "bg-[red]": recorder?.recording,
+                  "h-14 w-36": recordedVideo,
+                }
+              )}
+              onClick={handleRecordControlClick}
+            >
+              {recordedVideo ? (
+                <span className="text-background-secondary">Rerecord</span>
+              ) : (
+                <div
+                  className={cn("size-6 bg-background-primary transition-all", {
+                    "rounded-full": !recorder?.recording,
+                    "rounded-md": recorder?.recording,
+                  })}
+                />
+              )}
+            </button>
+            {!recordedVideo && (
+              <div
+                className={cn("rounded-full py-1 px-2", {
+                  "bg-background-primary": !recorder?.recording,
+                  "bg-[red] text-background-secondary": recorder?.recording,
+                })}
+              >
+                {formatDuration(recorder?.duration || 0)}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/form/steps/video-feedback/video-recorder.tsx
@@ -0,0 +1,85 @@
+import { useEffect, useMemo, useRef, useState } from "react";
+
+export interface UseRecorderConfig {
+  enabled: boolean;
+  onVideo: (video: Blob) => void;
+}
+
+export const useRecorder = ({ enabled, onVideo }: UseRecorderConfig) => {
+  const [stream, setStream] = useState<MediaStream | null>(null);
+  const [recording, setRecording] = useState(false);
+  const [duration, setDuration] = useState(0); // Track recording duration
+  const intervalRef = useRef<NodeJS.Timeout | null>(null); // Ref to store interval ID
+
+  useEffect(() => {
+    if (!enabled) return;
+
+    if (!window.navigator.mediaDevices) {
+      return console.warn(
+        "MediaDevices is only available in a secure context."
+      );
+    }
+
+    let internalStream: MediaStream | null = null;
+
+    window.navigator.mediaDevices
+      .getUserMedia({
+        video: { facingMode: "user" },
+        audio: true,
+      })
+      .then((stream) => setStream((internalStream = stream)));
+
+    return () => {
+      internalStream?.getTracks().forEach((track) => track.stop());
+    };
+  }, [enabled]);
+
+  const recorder = useMemo(() => {
+    if (!stream) return null;
+
+    let recordedChunks: Blob[] = [];
+
+    const recorder = new MediaRecorder(stream);
+
+    recorder.ondataavailable = (e) => {
+      if (e.data.size) recordedChunks.push(e.data);
+    };
+
+    recorder.onstart = () => {
+      setRecording(true);
+      setDuration(0); // Reset duration when recording starts
+
+      // Start timer
+      intervalRef.current = setInterval(() => {
+        setDuration((prev) => prev + 1);
+      }, 1000);
+    };
+
+    recorder.onstop = () => {
+      setRecording(false);
+      onVideo?.(new Blob(recordedChunks, { type: "video/webm" }));
+      recordedChunks = [];
+      // Stop timer
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+      }
+      setDuration(0); // Reset duration when recording starts
+    };
+
+    const start = () => recorder.start();
+    const stop = () => recorder.stop();
+
+    return { start, stop };
+  }, [stream]);
+
+  if (!recorder) return null;
+
+  return {
+    start: recorder.start,
+    stop: recorder.stop,
+    recording,
+    duration,
+    stream,
+  };
+};
