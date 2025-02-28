6b4eb57a875665eaeacd8b28458bec1991d560a3
JuliusNiedermeier <julius.niedermeier@outlook.de>
2025-02-28 15:32:04 +0100

    Added slide animation to step-carousel

--- src/components/form/step-carousel.tsx
+++ src/components/form/step-carousel.tsx
@@ -1,8 +1,19 @@
 "use client";
 
-import { ComponentProps, FC } from "react";
+import {
+  ComponentProps,
+  FC,
+  useDeferredValue,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { useForm } from ".";
 import { cn } from "@/utils/cn";
+import { AnimatePresence, motion } from "motion/react";
+
+const animationDistance = 500;
 
 export type StepCarouselProps = Omit<ComponentProps<"div">, "children">;
 
@@ -11,15 +22,64 @@ export const StepCarousel: FC<StepCarouselProps> = ({
   ...restProps
 }) => {
   const { steps, space } = useForm();
+  const [containerWidth, setContaienerWidth] = useState<number | null>(null);
+  const deferredCurrentStepIndex = useDeferredValue(space?.currentStepIndex);
+  const previousStepIndex = useRef<number | undefined>(undefined);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const direction = useMemo(() => {
+    if (
+      space?.currentStepIndex === undefined ||
+      previousStepIndex.current === undefined ||
+      space.currentStepIndex === previousStepIndex.current
+    ) {
+      return 0;
+    }
+
+    return space.currentStepIndex > previousStepIndex.current ? 1 : -1;
+  }, [space?.currentStepIndex]);
+
+  previousStepIndex.current = space?.currentStepIndex;
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const observer = new ResizeObserver(([{ contentRect }]) => {
+      setContaienerWidth(contentRect.width);
+    });
+
+    observer.observe(containerRef.current);
+
+    return () => observer.disconnect();
+  }, [containerRef.current]);
+
+  const animationDistance = useMemo(() => {
+    if (!containerWidth) return 0;
+    return containerWidth + 20;
+  }, [containerWidth]);
+
   if (space?.currentStepIndex === undefined) return;
 
   return (
     <div
-      className={cn("flex-1 flex flex-col justify-end", className)}
+      ref={containerRef}
+      className={cn("flex-1 relative", className)}
       {...restProps}
     >
-      {typeof space.currentStepIndex === "number" &&
-        steps[space.currentStepIndex].component}
+      <AnimatePresence>
+        {typeof deferredCurrentStepIndex === "number" && (
+          <motion.div
+            className="absolute top-0 left-0 w-full h-full flex-1 flex flex-col justify-end"
+            transition={{ type: "tween", ease: "easeInOut", duration: 0.25 }}
+            key={deferredCurrentStepIndex}
+            initial={{ opacity: 0, x: animationDistance * direction }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -animationDistance * direction }}
+          >
+            {steps[deferredCurrentStepIndex].component}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
