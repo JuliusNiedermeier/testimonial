caf868207c33c0390d839116c5d84f59fc28d86e
JuliusNiedermeier <julius.niedermeier@outlook.de>
2025-03-08 21:27:20 +0100

    Added early return for AccountLink

--- src/app/(dashboard)/_components/navigation/account-link.tsx
+++ src/app/(dashboard)/_components/navigation/account-link.tsx
@@ -5,10 +5,12 @@ import Image from "next/image";
 export const AccountLink: FC = async () => {
   const session = await getSession();
 
+  if (!session) return null;
+
   return (
     <div className="p-6 flex gap-4 items-center hover:bg-background-secondary text-left overflow-hidden">
       <div className="shrink-0 size-12 relative bg-background-secondary rounded-full overflow-hidden">
-        {session?.user.image && (
+        {session.user.image && (
           <Image
             src={session.user.image}
             alt={session.user.name}
@@ -18,9 +20,9 @@ export const AccountLink: FC = async () => {
         )}
       </div>
       <div className="overflow-hidden">
-        <span className="text-label truncate block">{session?.user.name}</span>
+        <span className="text-label truncate block">{session.user.name}</span>
         <span className="text-foreground-secondary truncate block">
-          {session?.user.email}
+          {session.user.email}
         </span>
       </div>
     </div>
