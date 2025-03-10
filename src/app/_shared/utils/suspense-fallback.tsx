9048cd9ccdb64bf64e57b62dd42b015d26063ff3
JuliusNiedermeier <julius.niedermeier@outlook.de>
2025-03-10 19:42:39 +0100

    Added utils for rendering async components with Suspense and fallback UI

---
+++ src/app/_shared/utils/suspense-fallback.tsx
@@ -0,0 +1,56 @@
+import { FC, ReactNode, Suspense } from "react";
+
+export type SyncFC<P = object> = FC<P> & {
+  (props: P): Exclude<ReactNode, Promise<unknown>>;
+};
+
+export type WithSuspended<Props = object, SuspendedProps = object> =
+  | ({ suspended: true } & SuspendedProps)
+  | ({ suspended: false } & Props);
+
+export const withSuspense = <Props extends object = object>(config: {
+  Component: FC<Props>;
+  Fallback: SyncFC<Props>;
+}) => {
+  const WithSuspense = (props: Props) => {
+    return (
+      <Suspense fallback={<config.Fallback {...props} />}>
+        <config.Component {...props} />
+      </Suspense>
+    );
+  };
+
+  return WithSuspense;
+};
+
+interface WithSuspenseFallbackConfig<
+  AsyncProps extends object,
+  UIProps extends object
+> {
+  AsyncComponent: FC<{ UIComponent: FC<UIProps> } & AsyncProps>;
+  UIComponent: SyncFC<WithSuspended<UIProps, AsyncProps>>;
+}
+
+export const withSuspenseFallback = <
+  AsyncProps extends object,
+  UIProps extends object
+>(
+  config: WithSuspenseFallbackConfig<AsyncProps, UIProps>
+) => {
+  const AsyncComponent = (props: AsyncProps) => (
+    <config.AsyncComponent
+      UIComponent={(uiProps) => (
+        <config.UIComponent suspended={false} {...uiProps} />
+      )}
+      {...props}
+    />
+  );
+
+  return Object.assign(
+    withSuspense({
+      Component: AsyncComponent,
+      Fallback: (props) => <config.UIComponent suspended={true} {...props} />,
+    }),
+    { Async: AsyncComponent, UI: config.UIComponent }
+  );
+};
