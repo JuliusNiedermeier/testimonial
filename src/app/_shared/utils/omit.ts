6951d96b49eb29124670fc89ab563aa41c3dc6e9
JuliusNiedermeier <julius.niedermeier@outlook.de>
2025-03-15 10:38:54 +0100

    Added the new pick function that was accidentally not added to the previous commit.

--- src/app/_shared/utils/omit.ts
+++ src/app/_shared/utils/omit.ts
@@ -8,3 +8,12 @@ export const omit = <Source extends object, Keys extends Array<keyof Source>>(
     )
   ) as Omit<Source, Keys[number]>;
 };
+
+export const pick = <Source extends object, Keys extends Array<keyof Source>>(
+  source: Source,
+  keys: Keys
+) => {
+  return Object.fromEntries(
+    Object.entries(source).filter(([key]) => keys.includes(key as keyof Source))
+  ) as Pick<Source, Keys[number]>;
+};
