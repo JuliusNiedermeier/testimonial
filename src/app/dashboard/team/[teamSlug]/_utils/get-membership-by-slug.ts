a95cf4a871b713c24ef6c0970180ae367daba88f
JuliusNiedermeier <julius.niedermeier@outlook.de>
2025-03-30 21:02:50 +0200

    Cached get-membership-by-slug

--- src/app/dashboard/team/[teamSlug]/_utils/get-membership-by-slug.ts
+++ src/app/dashboard/team/[teamSlug]/_utils/get-membership-by-slug.ts
@@ -1,4 +1,5 @@
 import { Membership } from "@prisma/client";
+import { unstable_cacheTag } from "next/cache";
 import { redirect } from "next/navigation";
 import { cache } from "react";
 import { db } from "root/src/app/_db";
@@ -15,11 +16,17 @@ export type GetMembershipBySlugResult<R extends boolean> = R extends true
 
 export const getMembershipBySlug = cache(
   async <R extends boolean>(config: GetMembershipBySlugConfig<R>) => {
+    "use cache";
+
     const membership = await db.membership.findFirst({
       where: { userId: config.userId, team: { slug: config.teamSlug } },
     });
 
     if (config.require && !membership) redirect("/login");
+
+    if (membership) {
+      unstable_cacheTag(`membership:${membership.teamId}:${membership.userId}`);
+    }
 
     return membership;
   }
